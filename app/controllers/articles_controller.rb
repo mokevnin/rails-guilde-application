e7ec1ac0b1b23474bb19a018e966352f244c51c8
Kirill Mokevnin <mokevnin@gmail.com>
2018-02-03 15:47:24 +0700

    remove auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,5 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "viraj", password: "password", except: [:index, :show]
-
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "viraj", password: "password", only: :destroy
-
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
